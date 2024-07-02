e52f2e040ace1efdd96fdd7cfea74194a2f5c87b
anshikaa-jain <168558155+anshikaa-jain@users.noreply.github.com>
2024-07-03 00:34:26 +0530

    Updated

--- single_inverted/single_inverted/balance.py
+++ single_inverted/single_inverted/balance.py
@@ -8,42 +8,54 @@ import numpy as np
 
 from custom_msgs.msg import TorqueInput, States
 
-Kp = 0.2
-Kd = 0.001
-Ki = 4.0
-
+Kp = 0.2  
+Kd = 3.0  
+Ki = 0.001  
+dt = 1/500
 class single_inverted_pendulum_balance(Node):
     
     def __init__(self):  
         self.prev_error = 0 
         self.integral_error = 0        
 
-        super().__init__('simple inverted pendulum balancer')
+        super().__init__('simple_inverted_pendulum_balancer')
         self.subscription = self.create_subscription(States,'/state_feedback',self.balance ,10) 
         self.publisher = self.create_publisher(TorqueInput,'/torque_input',10 )
         self.prev_time = self.get_clock().now()
 
-    def time_diff(self, time1: Time, time2 : Time):
-        return (time1.nanoseconds-time2.nanoseconds)/10e9
+    def time_diff(self, time1: Time, time2: Time) -> float:
+        time1_msg = time1.to_msg()
+        time2_msg = time2.to_msg()
+        sec_diff = time1_msg.sec - time2_msg.sec
+        nanosec_diff = time1_msg.nanosec - time2_msg.nanosec
+        return (sec_diff + nanosec_diff / 1e9)
 
     def balance(self, msg: States):
-        measured_value = msg.theta
+        measured_value = abs(msg.theta)
         current_time = self.get_clock().now()
         error = np.pi - measured_value
         
         self.integral_error = self.integral_error + error
-        derivative_error = (error - self.prev_error)/self.time_diff(current_time, self.prev_error)
+        derivative_error = (error - self.prev_error)/ self.time_diff(current_time,self.prev_time)
+
 
         change = Kp*error + Ki*self.integral_error + Kd*derivative_error
 
-        torque_input = TorqueInput()
-        torque_input.torque_value = measured_value + change
-        if msg.theta < 0:
-            torque_input.torque_value *= -1
-        self.publisher.publish(torque_input)
-
         self.prev_error = error
         self.prev_time = current_time
+
+        torque_input = TorqueInput()
+        torque_input.torque_value = measured_value + change
+
+        if msg.theta < 0:
+            torque_input.torque_value *= -1
+        if torque_input.torque_value > 5:
+            torque_input.torque_value = 5.0
+        if torque_input.torque_value < -5:
+            torque_input.torque_value = -5.0
+        self.publisher.publish(torque_input)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
